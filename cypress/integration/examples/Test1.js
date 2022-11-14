f72ce059d292a13d941b9d5ca0d5d1059d39bbec
Aurora Guadalupe Escalera Arroyo <aurora.escalera@softtekprojects.com>
2022-11-14 16:06:51 -0600

    TEST 1: This test visits an ecommerce, search the word 'ca' on the input bar, compares the title's card with the word 'Cashews' and add the product to the cart.

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,5 +1,24 @@
+/*
+TEST 1
+This test case go to an ecommerce, search for words with 'ca', search for 'cashews' 
+on the results and adds the product by clicking on the card.
+1. Go to url: https://rahulshettyacademy.com/seleniumPractise/#/.
+2. Type on the input search bar the word 'ca'.
+3. Save cards shown with a variable called 'productLocator'.
+4. Start a forEach loop (@productLocator) to find the card of 'Cashews' and click on the button to add.  
+*/
+
 describe('My first test suite', function(){
     it('', function(){
         cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+        cy.get('.search-keyword').type('ca');
+        cy.wait(2000);
+        cy.get('.product').as('productLocator');  
+        cy.get('@productLocator').each((el, index, list)=>{
+            const textVeg = el.find('h4.product-name').text();
+            if(textVeg.includes('Cashews')){
+                cy.wrap(el).find('button').click();
+            };
+        }); 
     });
 });
