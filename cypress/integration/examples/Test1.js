fd2af60dd35f07bc30689d40507191bb494fe052
Aurora Guadalupe Escalera Arroyo <aurora.escalera@softtekprojects.com>
2022-11-14 16:55:49 -0600

    This test case go to an ecommerce, search for words with 'ca', search for 'cashews'

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,11 +1,10 @@
 /*
 TEST 1
 This test case go to an ecommerce, search for words with 'ca', search for 'cashews' 
-on the results and adds the product by clicking on the card.
+and makes sure the length is four
 1. Go to url: https://rahulshettyacademy.com/seleniumPractise/#/.
 2. Type on the input search bar the word 'ca'.
-3. Save cards shown with a variable called 'productLocator'.
-4. Start a forEach loop (@productLocator) to find the card of 'Cashews' and click on the button to add.  
+3. Assert if .product are 4 of length
 */
 
 describe('My first test suite', function(){
@@ -13,12 +12,6 @@ describe('My first test suite', function(){
         cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
         cy.get('.search-keyword').type('ca');
         cy.wait(2000);
-        cy.get('.product').as('productLocator');  
-        cy.get('@productLocator').each((el, index, list)=>{
-            const textVeg = el.find('h4.product-name').text();
-            if(textVeg.includes('Cashews')){
-                cy.wrap(el).find('button').click();
-            };
-        }); 
+        cy.get('.product').should('have.length',4);
     });
 });
--- cypress/integration/examples/Test3.js
+++ cypress/integration/examples/Test3.js
@@ -1,25 +1,18 @@
 /*
-TEST 2
+TEST 3
 HANDLING ASYNC PROMISES WITH CYPRESS AND PRINT TEXT.
 
 This test case go to an ecommerce, search for words with 'ca', click on the fourth child of the products 
 object and click on its 'add to cart' button. Test if the length of these products is four and if the 
 second product contains 'add to cart' text and click it, resolving its asynchrony with .then.
 
-search for 'cashews' 
-on the results and adds the product by clicking on the card.
 1. Go to url: https://rahulshettyacademy.com/seleniumPractise/#/.
 2. Type on the input search bar the word 'ca'.
-3. Save shown cards in class 'products'  with a variable called 'productLocator'.
-4. Make sure the products shown have length of 4.
-5. Select the fourth child of this @productClass object and click on the button.
-6. Test if the second product of @productLocator contains "Add TO CART" text and click
-   resolve the asyncrony with .then-> console.log('sf')
-
-To test logo's text is correctly displayed:
-7. Check if the object of the class .brand has the text 'GREENKART'.
+3. Save cards shown with a variable called 'productLocator'.
+4. Start a forEach loop (@productLocator) to find the card of 'Cashews' and click on the button to add.
 */
 
+///<reference  types="Cypress"/>
 const cypress = require("cypress");
 
 describe('', function(){
@@ -27,10 +20,12 @@ describe('', function(){
         cypress.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
         cy.get('search-keyword').type('ca');
         cy.get('.products').as('productLocator');
-        cy.get('@productLocator').find('product').should('have.length',4);
-        cy.get(':nth.child(3)>.product-action>button').click();
-        cy.get('@productLocator').find('.product').eq(2).contains('ADD TO CART').click().then(function(){
-            console.log('sf');
+        cy.get('@productLocator').find('.product').each((el,index,list)=>{
+            const textVeg = el.find('h4.product-name').text();
+            if(textVeg.includes('Cashews')){
+                wrap(el).find('button').click();
+            };
+            cy.get('.cart-icon>img').click();
         });
 
         //assert if logo text is correctly displayed
