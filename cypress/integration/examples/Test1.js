497f5956cb17b571c4479df5a979957435598d5d
Aurora Guadalupe Escalera Arroyo <aurora.escalera@softtekprojects.com>
2022-11-14 12:30:19 -0600

    First commit with first Cypress project. Test1: Visit rahulshetty page

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,12 +1,5 @@
-//cypress - Spec
-///<reference types="Cypress"/>
-describe('My First Test Suite',function(){
-    it('My First Test Case', function(){
+describe('My first test suite', function(){
+    it('', function(){
         cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
-        cy.get('.search-keyword').type('ca');
-        cy.wait(2000);
-        //cy.get('.product:visible').should('have.length',4);
-        //Parent Child Chaining
-        cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click();
-    })
-})
+    });
+});
