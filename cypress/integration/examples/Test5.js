e97673579947c0faafdea51c37776e5a763692b4
Aurora Guadalupe Escalera Arroyo <aurora.escalera@softtekprojects.com>
2022-11-16 08:55:12 -0600

    TEST 6: STATIC DROPDOWNS USING SELECT COMMAND  This test select an option of a static dropdown and verify if it has the value 'option2'.

--- cypress/integration/examples/Test5.js
+++ cypress/integration/examples/Test5.js
@@ -1,7 +1,9 @@
 /*
 TEST 5: VERY AND AUTOMATE CHECKBOX
-This test case go to an automation practice page, checks a specific checkbox, verifies if it's checked and its value. It cheks the box again and now verifies if it is unchecked, and finally checks two boxes at the same time.
-1. Go to url: https://rahulshettyacademy.com/automationPractice/#/.
+This test case go to an automation practice page, checks a specific checkbox, verifies if it's checked and 
+its value. It cheks the box again and now verifies if it is unchecked, and finally checks two boxes at the 
+same time.
+1. Go to url: https://rahulshettyacademy.com/AutomationPractice/.
 2. Check '#checkboxOption1' box and verifies if it is checked and its value should be 'Option1'.
 3. Uncheck '#checkboxOption1' box and verifies if it is unchecked.
 4. Check option2 and option3 at the same time.
---
+++ cypress/integration/examples/Test6.js
@@ -0,0 +1,17 @@
+/*
+TEST 6: STATIC DROPDOWNS USING SELECT COMMAND
+This test select an option of a static dropdown and verify if it has the value 'option2'.
+
+1. Go to url: https://rahulshettyacademy.com/AutomationPractice/.
+2. Select the dropdown, use the select function to choose 'option2' and
+verify that its value is correctly 'option2'.
+
+*/
+
+///<reference types="cypress"/>
+describe('',function(){
+    it('',function(){
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
+        cy.get('select').select('option2').should('have.value','option2');
+    })
+})
